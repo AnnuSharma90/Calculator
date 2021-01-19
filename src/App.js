f25f65d42a416be9826f9cca94936b26fbf4b7f2
Annu <54701665+AnnuSharma90@users.noreply.github.com>
2021-01-19 11:10:08 +0530

    calculator submitted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import 'tachyons';
 
-function App() {
+
+const App=()=> {
+
+  const [num1, setNum1]=useState(0);
+  const [num2, setNum2]=useState(0);
+  const [Result, setResult]=useState('Result');
+
+
+    
+    const addition=()=>{
+      setResult(num1+num2)
+      
+    }
+
+    const substraction=()=>{
+      setResult(num1-num2)
+    }
+
+    const multiply=()=>{
+      setResult(num1*num2)
+    }
+
+    const divide=()=>{
+      setResult(num1/num2)
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1 class="f2 lh-title tc">CALCULATOR</h1>
+    <div className='br2 ba b--mid-gray bg-gray mv4 w-100 w-50-m w-25-l mw6 center shadow-5 ba bw2 bg-animate hover-bg-lightest-blue grow'>
+      <center>
+      <input className='ma2' type='number' value={num1} name='num1' onChange={e=>setNum1(Number(e.target.value))}/>
+      <input className='ma2' type='number' value={num2} name='num2' onChange={e=>setNum2(Number(e.target.value))}/>
+    <div>
+      
+      <button className='f6  ba bw1 mb2 mr1 dib mid-gray' value='+' onClick={addition}>+</button>
+      <button className='f6  ba bw1 mb2 mr1 dib mid-gray' value='-' onClick={substraction}>-</button>
+      <button className='f6  ba bw1 mb2 mr1 dib mid-gray' value='*' onClick={multiply}>*</button>
+      <button className='f6  ba bw1 mb2 mr1 dib mid-gray' value='/' onClick={divide}>/</button>
     </div>
+    <div>
+      <h2 className='dib'>{Result}</h2>
+    </div>
+    </center> 
+    </div>
+    </>
   );
-}
+};
 
 export default App;
